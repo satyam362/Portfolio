af17f027aa326eaca8df452373a497669f61a54c
satyam <satyamprasadsingh55@gmail.com>
2025-04-26 15:42:17 +0530

    saa

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -13,7 +13,7 @@ export const certifications: Certification[] = [
     title: 'Object-Oriented Programming',
     issuer: 'neocolab',
     date: '2023',
-    certificateLink: '',
+    certificateLink: '#',
     logo: 'https://images.pexels.com/photos/6802042/pexels-photo-6802042.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
   {
